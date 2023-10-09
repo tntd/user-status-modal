4148017d6da2cdb59f440ea1ef1c023c53ed5f05
周泽飞 <zefei.zhou@tongdun.net>
2023-10-09 11:35:01 +0800

    修复中英文翻译问题

--- dist/sources/lang/en_US/index.js
+++ dist/sources/lang/en_US/index.js
@@ -7,7 +7,7 @@ exports.default = void 0;
 var _default = {
   "noOperateContent": "You haven't performed the operation for a long time, the system has exited automatically",
   "otherLoginContent": "Current account is logged in on other devices",
-  "warnMsg": "Click OK to log in again",
+  "warnMsg": "Click ok to login again",
   "submit": "Ok"
 };
 exports.default = _default;
